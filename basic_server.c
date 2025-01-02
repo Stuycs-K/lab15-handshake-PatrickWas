90b8e26d426969b2fc8364e4a72418e4e5823740
PatrickWas <pwas50@stuy.edu>
2025-01-02 18:46:09 -0500

    checking if everything is working

--- basic_server.c
+++ basic_server.c
@@ -5,4 +5,5 @@ int main() {
   int from_client;
 
   from_client = server_handshake( &to_client );
+  // just write/read a byte to/from the server and test if that works 
 }
--- pipe_networking.c
+++ pipe_networking.c
@@ -16,14 +16,14 @@ int server_setup() {
     exit(1);
   }
 
-  //from_client = open(WKP, O_WRONLY)
-  /* if(from_client == -1){
+  from_client = open(WKP, O_RDONLY);
+  if(from_client == -1){
     perror("Error opening MARIO WKP pipe");
     exit(1);
-} */
-  // oh this probably requires all the other functions first :(
+  }
 
-  // additionally, make sure that I have a signal catcher for sigInt so that it deletes the pipe laterrrrrrr
+  unlink(WKP);
+
   return from_client;
 }
 
@@ -37,8 +37,24 @@ int server_setup() {
   returns the file descriptor for the upstream pipe (see server setup).
   =========================*/
 int server_handshake(int *to_client) {
-  // calls server_setup() and I think something calls something else?  
-  int from_client;
+  int from_client = server_setup();
+
+  char buffer[256];
+  if(read(from_client, buffer, sizeof(buffer)) == -1){
+    perror("Error reading from client");
+    exit(1);
+  }
+
+  *to_client = open(buffer, O_WRONLY);
+  if(*to_client == -1){
+    perror("Error opening pipe");
+    exit(1);
+  }
+
+  if(write(*to_client, *SYN_ACK, 4) == -1){
+    perror("Error sending SYN_ACK to client");
+    exit(1);
+  }
   return from_client;
 }
 
@@ -54,6 +70,39 @@ int server_handshake(int *to_client) {
   =========================*/
 int client_handshake(int *to_server) {
   int from_server;
+  char buffer[256]; 
+
+  snprintf(buffer, sizeof(buffer), "/tmp/%d", getpid());
+
+  if(mkfifo(buffer, 0666) == -1){
+    perror("Error creating client's PP");
+    exit(1);
+  }
+
+  *to_server = open(WKP, O_WRONLY);
+  if(*to_server == -1){
+    perror("Error opening MARIO WKP");
+    exit(1);
+  }
+
+  if(write(*to_server, buffer, strlen(buffer) + 1) == -1){
+    perror("Error communicating with client pipe");
+    exit(1);
+  }
+
+  from_server = open(buffer, O_RDONLY);
+  if(from_server == -1){
+    perror("Error opening client's PP for reading");
+    exit(1);
+  }
+
+  if(read(from_server, buffer, sizeof(buffer)) == -1){
+    perror("Error reading from server");
+    exit(1);
+  }
+
+  printf("Client got handshake: %s\n", buffer);
+
   return from_server;
 }
 
@@ -67,6 +116,21 @@ int client_handshake(int *to_server) {
   returns the file descriptor for the downstream pipe.
   =========================*/
 int server_connect(int from_client) {
-  int to_client  = 0;
+  int to_client;
+  char buffer[256];
+
+  if(read(from_client, buffer, sizeof(buffer)) == -1){
+    perror("Error reading client PP");
+    exit(1);
+  }
+
+  printf("Server got client PP: %s\n", buffer);
+
+  to_client = open(buffer, O_WRONLY);
+  if(to_client == -1){
+    perror("Error opening client's PP");
+    exit(1);
+  }
+
   return to_client;
 }
