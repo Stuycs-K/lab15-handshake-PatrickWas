f06295be2e174a47d6fd2cea76cc57a856539965
PatrickWas <pwas50@stuy.edu>
2025-01-08 07:45:19 -0500

    removed basic_server.c

--- basic_server.c
+++
@@ -1,9 +0,0 @@
-#include "pipe_networking.h"
-
-int main() {
-  int to_client;
-  int from_client;
-
-  from_client = server_handshake( &to_client );
-  // just write/read a byte to/from the server and test if that works 
-}
